e0f0ab839123ae31704f42cb63b53be4387c6016
TuqMt <forostanmatvej74@gmail.com>
2025-06-06 19:40:38 +0500

    Update middle_grade.py

--- examples/middle_grade.py
+++ examples/middle_grade.py
@@ -1,9 +1,13 @@
 from core import mystat_auth
+# Создаём экземпляр без прокси
 client = mystat_auth('my_login', 'my_password')
 
+# Вызываем метод для подсчёта среднего балла
 average = client.middlemark()
 
+# Если средний балл посчитан успешно — выводим его
 if average is not None:
     print(f"Средний балл: {average:.2f}")
 else:
     print("Ошибка при вычислении среднего балла.")
+
